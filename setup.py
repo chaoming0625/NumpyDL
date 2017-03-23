1ff11a511950065d27a37ef3092494bc72e2173e
oujago@gmail.com <npdl2017>
2017-03-23 10:54:49 +0800

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,37 @@
 # -*- coding: utf-8 -*-
 
 """
-@author: ChaoMing (https://oujago.github.io/)
-
-@date: Created on 17-1-10
+@author: Chao-Ming Wang (https://oujago.github.io/)
 
 @notes:
-    just run "python setup.py build_ext --inplace" in command
+    run "python setup.py build_ext --inplace" in command
 """
 
 from distutils.core import setup
 
 import numpy as np
-from Cython.Build import cythonize
+from setuptools import find_packages
+
+version = '0.1'
+
+description = """
+Deep Learning Framework based on Numpy
+"""
 
 setup(
-    name='pydl',
-    ext_modules=cythonize(
-        [
-            'pydl/backend/conv.pyx',
-            'pydl/backend/pool.pyx'
-        ]
-    ),
-    include_dirs=[np.get_include()]
+    name='npdl',
+    version=version,
+    description=description,
+    author='Chao-Ming Wang',
+    packages=find_packages(),
+    author_email='oujago@gmail.com',
+    url='https://oujago.github.io/',
+    include_dirs=[np.get_include()],
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'xlwt',
+        'scikit-learn',
+        'scipy'
+    ]
 )
