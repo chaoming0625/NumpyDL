97e9145e13b86e28487ceb992b0b16e8061e00ec
chaoming <ouajgo@gmail.com>
2017-06-05 00:29:00 +0800

    unk

--- docs/conf.py
+++ docs/conf.py
@@ -125,7 +125,11 @@ def setup(app):
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'collapse_navigation': False,
+    'display_version': False,
+    'navigation_depth': 3,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
