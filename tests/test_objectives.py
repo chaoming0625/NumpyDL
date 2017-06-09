daa8a35d8465e146717b0fcf7ad3187578e09a44
chaoming <ouajgo@gmail.com>
2017-06-09 23:00:35 +0800

    add test objective

--- tests/test_objectives.py
+++ tests/test_objectives.py
@@ -3,3 +3,19 @@
 import pytest
 
 
+def test_objective():
+    from npdl.objectives import Objective
+
+    obj = Objective()
+
+    with pytest.raises(NotImplementedError):
+        obj.forward(None, None)
+
+    with pytest.raises(NotImplementedError):
+        obj.backward(None, None)
+
+    assert str(obj) == 'Objective'
+
+
+
+
