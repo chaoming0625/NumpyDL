59012eeaea76e57e01d2198f0b8b410fbef0df81
chaoming <ouajgo@gmail.com>
2017-06-12 12:51:54 +0800

    parameters loading is done

--- applications/chatbot/model.py
+++ applications/chatbot/model.py
@@ -30,7 +30,7 @@ class Utils:
         return s
 
     @staticmethod
-    def load_params():
+    def load_params(hi):
         embed_words, \
         softmax_weights, \
         en_lstm1_W, en_lstm1_U, en_lstm1_b, \
@@ -38,7 +38,56 @@ class Utils:
         de_lstm1_W, de_lstm1_U, de_lstm1_b, \
         de_lstm2_W, de_lstm2_U, de_lstm2_b = np.load(param_path)
 
-        params = {'embed_words': embed_words,}
+        params = {'embed_words': embed_words,
+                  'en_lstm1': {'U_f': en_lstm1_W[:, : hi * 1],
+                               'U_i': en_lstm1_W[:, hi * 1: hi * 2],
+                               'U_o': en_lstm1_W[:, hi * 2: hi * 3],
+                               'U_g': en_lstm1_W[:, hi * 3:],
+                               'W_f': en_lstm1_U[:, : hi * 1],
+                               'W_i': en_lstm1_U[:, hi * 1: hi * 2],
+                               'W_o': en_lstm1_U[:, hi * 2: hi * 3],
+                               'W_g': en_lstm1_U[:, hi * 3:],
+                               'b_f': en_lstm1_b[: hi * 1],
+                               'b_i': en_lstm1_b[hi * 1: hi * 2],
+                               'b_o': en_lstm1_b[hi * 2: hi * 3],
+                               'b_g': en_lstm1_b[hi * 3:]},
+                  'en_lstm2': {'U_f': en_lstm2_W[:, : hi * 1],
+                               'U_i': en_lstm2_W[:, hi * 1: hi * 2],
+                               'U_o': en_lstm2_W[:, hi * 2: hi * 3],
+                               'U_g': en_lstm2_W[:, hi * 3:],
+                               'W_f': en_lstm2_U[:, : hi * 1],
+                               'W_i': en_lstm2_U[:, hi * 1: hi * 2],
+                               'W_o': en_lstm2_U[:, hi * 2: hi * 3],
+                               'W_g': en_lstm2_U[:, hi * 3:],
+                               'b_f': en_lstm2_b[: hi * 1],
+                               'b_i': en_lstm2_b[hi * 1: hi * 2],
+                               'b_o': en_lstm2_b[hi * 2: hi * 3],
+                               'b_g': en_lstm2_b[hi * 3:]},
+                  'de_lstm1': {'U_f': de_lstm1_W[:, : hi * 1],
+                               'U_i': de_lstm1_W[:, hi * 1: hi * 2],
+                               'U_o': de_lstm1_W[:, hi * 2: hi * 3],
+                               'U_g': de_lstm1_W[:, hi * 3:],
+                               'W_f': de_lstm1_U[:, : hi * 1],
+                               'W_i': de_lstm1_U[:, hi * 1: hi * 2],
+                               'W_o': de_lstm1_U[:, hi * 2: hi * 3],
+                               'W_g': de_lstm1_U[:, hi * 3:],
+                               'b_f': de_lstm1_b[: hi * 1],
+                               'b_i': de_lstm1_b[hi * 1: hi * 2],
+                               'b_o': de_lstm1_b[hi * 2: hi * 3],
+                               'b_g': de_lstm1_b[hi * 3:]},
+                  'de_lstm2': {'U_f': de_lstm2_W[:, : hi * 1],
+                               'U_i': de_lstm2_W[:, hi * 1: hi * 2],
+                               'U_o': de_lstm2_W[:, hi * 2: hi * 3],
+                               'U_g': de_lstm2_W[:, hi * 3:],
+                               'W_f': de_lstm2_U[:, : hi * 1],
+                               'W_i': de_lstm2_U[:, hi * 1: hi * 2],
+                               'W_o': de_lstm2_U[:, hi * 2: hi * 3],
+                               'W_g': de_lstm2_U[:, hi * 3:],
+                               'b_f': de_lstm2_b[: hi * 1],
+                               'b_i': de_lstm2_b[hi * 1: hi * 2],
+                               'b_o': de_lstm2_b[hi * 2: hi * 3],
+                               'b_g': de_lstm2_b[hi * 3:]},
+                  'softmax': softmax_weights}
 
         return params
 
@@ -79,43 +128,125 @@ class Utils:
         else:
             return s[:fid - 1]
 
+    @staticmethod
+    def get_mask(data):
+        mask = (np.not_equal(data, 0)).astype("int32")
+        return mask
+
 
 class Seq2Seq:
     def __init__(self, hidden_size=512, nb_seq=max_sent_size):
         print("Load parameters ...")
-        params = Utils.load_params()
+        params = Utils.load_params(hidden_size)
         print('Loading is done.')
 
         # embedding
         self.embedding = npdl.layers.Embedding(params['embed_words'], nb_seq=nb_seq)
         self.embedding.connect_to()
+        self.embedding.embed_words = params['embed_words']
 
         # encoder LSTM 1
-        self.encoder_lstm1 = npdl.layers.BatchLSTM(n_out=hidden_size, n_in=hidden_size,
-                                                   return_sequence=True, nb_seq=nb_seq)
+        self.encoder_lstm1 = npdl.layers.LSTM(n_out=hidden_size, n_in=hidden_size,
+                                              return_sequence=True, nb_seq=nb_seq)
         self.encoder_lstm1.connect_to(self.embedding)
+        self.encoder_lstm1.U_f = params['en_lstm1']['U_f']
+        self.encoder_lstm1.U_i = params['en_lstm1']['U_i']
+        self.encoder_lstm1.U_o = params['en_lstm1']['U_o']
+        self.encoder_lstm1.U_g = params['en_lstm1']['U_g']
+        self.encoder_lstm1.W_f = params['en_lstm1']['W_f']
+        self.encoder_lstm1.W_i = params['en_lstm1']['W_i']
+        self.encoder_lstm1.W_o = params['en_lstm1']['W_o']
+        self.encoder_lstm1.W_g = params['en_lstm1']['W_g']
+        self.encoder_lstm1.b_f = params['en_lstm1']['b_f']
+        self.encoder_lstm1.b_i = params['en_lstm1']['b_i']
+        self.encoder_lstm1.b_o = params['en_lstm1']['b_o']
+        self.encoder_lstm1.b_g = params['en_lstm1']['b_g']
 
         # encoder LSTM 2
-        self.encoder_lstm2 = npdl.layers.BatchLSTM(n_out=hidden_size, return_sequence=False)
+        self.encoder_lstm2 = npdl.layers.LSTM(n_out=hidden_size, return_sequence=True)
         self.encoder_lstm2.connect_to(self.encoder_lstm1)
+        self.encoder_lstm2.U_f = params['en_lstm2']['U_f']
+        self.encoder_lstm2.U_i = params['en_lstm2']['U_i']
+        self.encoder_lstm2.U_o = params['en_lstm2']['U_o']
+        self.encoder_lstm2.U_g = params['en_lstm2']['U_g']
+        self.encoder_lstm2.W_f = params['en_lstm2']['W_f']
+        self.encoder_lstm2.W_i = params['en_lstm2']['W_i']
+        self.encoder_lstm2.W_o = params['en_lstm2']['W_o']
+        self.encoder_lstm2.W_g = params['en_lstm2']['W_g']
+        self.encoder_lstm2.b_f = params['en_lstm2']['b_f']
+        self.encoder_lstm2.b_i = params['en_lstm2']['b_i']
+        self.encoder_lstm2.b_o = params['en_lstm2']['b_o']
+        self.encoder_lstm2.b_g = params['en_lstm2']['b_g']
 
-        # # decoder LSTMs
-        # self.decoder_lstm1 = npdl.layers.BatchLSTM(n_out=hidden_size, n_in=hidden_size,
-        #                                            return_sequence=True)
-        # self.decoder_lstm2 = npdl.layers.BatchLSTM(n_out=hidden_size, return_sequence=False)
+        # decoder LSTM 1
+        self.decoder_lstm1 = npdl.layers.LSTM(n_out=hidden_size, n_in=hidden_size,
+                                              return_sequence=True)
+        self.decoder_lstm1.connect_to(self.embedding)
+        self.decoder_lstm1.U_f = params['de_lstm1']['U_f']
+        self.decoder_lstm1.U_i = params['de_lstm1']['U_i']
+        self.decoder_lstm1.U_o = params['de_lstm1']['U_o']
+        self.decoder_lstm1.U_g = params['de_lstm1']['U_g']
+        self.decoder_lstm1.W_f = params['de_lstm1']['W_f']
+        self.decoder_lstm1.W_i = params['de_lstm1']['W_i']
+        self.decoder_lstm1.W_o = params['de_lstm1']['W_o']
+        self.decoder_lstm1.W_g = params['de_lstm1']['W_g']
+        self.decoder_lstm1.b_f = params['de_lstm1']['b_f']
+        self.decoder_lstm1.b_i = params['de_lstm1']['b_i']
+        self.decoder_lstm1.b_o = params['de_lstm1']['b_o']
+        self.decoder_lstm1.b_g = params['de_lstm1']['b_g']
 
-    def forward(self, idxs):
+        # decoder LSTM 2
+        self.decoder_lstm2 = npdl.layers.LSTM(n_out=hidden_size, return_sequence=False)
+        self.decoder_lstm2.connect_to(self.decoder_lstm1)
+        self.decoder_lstm2.U_f = params['de_lstm2']['U_f']
+        self.decoder_lstm2.U_i = params['de_lstm2']['U_i']
+        self.decoder_lstm2.U_o = params['de_lstm2']['U_o']
+        self.decoder_lstm2.U_g = params['de_lstm2']['U_g']
+        self.decoder_lstm2.W_f = params['de_lstm2']['W_f']
+        self.decoder_lstm2.W_i = params['de_lstm2']['W_i']
+        self.decoder_lstm2.W_o = params['de_lstm2']['W_o']
+        self.decoder_lstm2.W_g = params['de_lstm2']['W_g']
+        self.decoder_lstm2.b_f = params['de_lstm2']['b_f']
+        self.decoder_lstm2.b_i = params['de_lstm2']['b_i']
+        self.decoder_lstm2.b_o = params['de_lstm2']['b_o']
+        self.decoder_lstm2.b_g = params['de_lstm2']['b_g']
+
+        # softmax layer
+        self.softmax = npdl.layers.Softmax(n_out=self.embedding.embed_words.shape[0])
+        self.softmax.connect_to(self.decoder_lstm2)
+        self.softmax.W = params['softmax']
+
+    def forward(self, idxs, masks):
+        # idxs data
+        idxs = idxs[None, :] if np.ndim(idxs) == 1 else idxs
+        masks = masks[None, :] if np.ndim(masks) == 1 else masks
+
+        # embedding words
+        embeds = self.embedding.forward(idxs)
+
+        # encoder lstm 1
+        en_lstm1_res = self.encoder_lstm1.forward(embeds, masks)
+
+        # encoder lstm 2
+        self.encoder_lstm2.forward(en_lstm1_res, masks)
+
+        # decoder lstm 1
+
+
+        # decoder lstm 2
+
+
+
         return idxs[::-1]
-
 
     def utter(self, sentence):
         # parse text to idxs
         idxs = np.asarray(Utils.cut_and_pad(Utils.tokens2idxs(Utils.tokenize(sentence)))[::-1], dtype='int32')
+        masks = Utils.get_mask(idxs)
 
-        idxs = self.forward(idxs)
+        idxs = self.forward(idxs, masks)
 
         # parse idxs to text
         tokens = Utils.idxs2tokens(idxs)
         sentence = Utils.cut_end(' '.join(tokens))
         return sentence
-
