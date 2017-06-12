43e75cb7f7747fefe2107adcb1adb4603f60da4f
chaoming <ouajgo@gmail.com>
2017-06-12 15:24:24 +0800

    add test_recurrent.py LSTM and GRU

--- tests/test_layers/test_recurrent.py
+++ tests/test_layers/test_recurrent.py
@@ -4,7 +4,8 @@
 import pytest
 import numpy as np
 from npdl.layers import Recurrent
-from npdl.layers import BatchLSTM
+from npdl.layers import GRU
+from npdl.layers import LSTM
 
 
 class PrevLayer:
@@ -21,4 +22,40 @@ def test_Recurrent():
             layer.connect_to()
         assert layer.connect_to(PrevLayer((20, 10, 100))) == 100
         assert len(layer.out_shape) == (3 if seq else 2)
+
+
+def test_GRU():
+
+    for seq in (True, False):
+        layer = GRU(n_out=200, n_in=100, return_sequence=seq)
+        assert layer.out_shape is None
+        layer.connect_to()
+        assert len(layer.out_shape) == (3 if seq else 2)
+
+        input = np.random.rand(10, 50, 100)
+        assert np.ndim(layer.forward(input)) == (3 if seq else 2)
+
+        with pytest.raises(NotImplementedError):
+            layer.backward(None)
+
+        assert len(layer.params) == 9
+        assert len(layer.grads) == 9
+
+
+def test_LSTM():
+    for seq in (True, False):
+        layer = LSTM(n_out=200, n_in=100, return_sequence=seq)
+        assert layer.out_shape is None
+        layer.connect_to()
+        assert len(layer.out_shape) == (3 if seq else 2)
+
+        input = np.random.rand(10, 50, 100)
+        mask = np.random.randint(0, 2, (10, 50))
+        assert np.ndim(layer.forward(input, mask)) == (3 if seq else 2)
+
+        with pytest.raises(NotImplementedError):
+            layer.backward(None)
+
+        assert len(layer.params) == 12
+        assert len(layer.grads) == 12
 
