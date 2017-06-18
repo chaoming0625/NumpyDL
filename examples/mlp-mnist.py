ef9a3ebb22db4adbcc18478736bed5691db60868
oujago@gmail.com <npdl2017>
2017-06-18 12:24:42 +0800

    add get_data to exmaple/mlp-mnist

--- examples/mlp-mnist.py
+++ examples/mlp-mnist.py
@@ -6,16 +6,22 @@ from sklearn.datasets import fetch_mldata
 
 import npdl
 
-# data
-print("loading data, please wait ...")
-mnist = fetch_mldata('MNIST original', data_home=os.path.join(os.path.dirname(__file__), './data'))
-print('data loading is done ...')
-X_train = mnist.data / 255.0
-y_train = mnist.target
-n_classes = np.unique(y_train).size
+
+def get_data():
+    # data
+    print("loading data, please wait ...")
+    mnist = fetch_mldata('MNIST original', data_home=os.path.join(os.path.dirname(__file__), './data'))
+    print('data loading is done ...')
+    X_train = mnist.data / 255.0
+    y_train = mnist.target
+    n_classes = np.unique(y_train).size
+
+    return n_classes, X_train, y_train
 
 
 def main(max_iter):
+    n_classes, X_train, y_train = get_data()
+
     # model
     print("building model ...")
     model = npdl.Model()
@@ -30,6 +36,8 @@ def main(max_iter):
 
 def main2(max_iter):
     # test Momentum optimizer
+
+    n_classes, X_train, y_train = get_data()
 
     # model
     print("building model ...")
@@ -46,6 +54,8 @@ def main2(max_iter):
 def main3(max_iter):
     # test NesterovMomentum optimizer
 
+    n_classes, X_train, y_train = get_data()
+
     # model
     print("building model ...")
     model = npdl.Model()
@@ -60,6 +70,8 @@ def main3(max_iter):
 
 def main4(max_iter):
     # test Adagrad optimizer
+
+    n_classes, X_train, y_train = get_data()
 
     # model
     print("building model ...")
@@ -76,6 +88,8 @@ def main4(max_iter):
 def main5(max_iter):
     # test RMSProp optimizer
 
+    n_classes, X_train, y_train = get_data()
+
     # model
     print("building model ...")
     model = npdl.Model()
@@ -90,6 +104,8 @@ def main5(max_iter):
 
 def main6(max_iter):
     # test Adadelta optimizer
+
+    n_classes, X_train, y_train = get_data()
 
     # model
     print("building model ...")
@@ -106,6 +122,8 @@ def main6(max_iter):
 def main7(max_iter):
     # test Adam optimizer
 
+    n_classes, X_train, y_train = get_data()
+
     # model
     print("building model ...")
     model = npdl.Model()
@@ -118,9 +136,10 @@ def main7(max_iter):
     model.fit(X_train, npdl.utils.data.one_hot(y_train), max_iter=max_iter, validation_split=0.1)
 
 
-
 def main8(max_iter):
     # test Adamax optimizer
+
+    n_classes, X_train, y_train = get_data()
 
     # model
     print("building model ...")
@@ -132,6 +151,7 @@ def main8(max_iter):
     # train
     print("train model ... ")
     model.fit(X_train, npdl.utils.data.one_hot(y_train), max_iter=max_iter, validation_split=0.1)
+
 
 if __name__ == '__main__':
     main8(50)
